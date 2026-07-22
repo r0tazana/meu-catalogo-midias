8d4f24bb133a54181b3234ae05535d8de94e7b02
Rodrigo <rodrigo.nif.oliveira@gmail.com>
2026-07-21 22:20:20 -0300

    Adicionei a função de cadastros de obras em csv

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from funcoes import inicializar_csv
+from funcoes import inicializar_csv, adicionar_itens 
 
 if __name__ == "__main__":
     print("=== INICIANDO SISTEMA DE CATÁLOGO ===")
     inicializar_csv()
+
+    #Chamada da função que criamos de captura de dados 
+    adicionar_itens()
--- funcoes.py
+++ funcoes.py
@@ -14,3 +14,46 @@ def inicializar_csv():
         print("Arquivo criado com sucesso!!")
     else:
         print("Banco de dados carregado com sucesso.")
+
+
+#função de capitura de dados do nosso catalogo de obras 
+
+def adicionar_itens():
+    print("====CADASTRO DE NOVA OBRA====")
+
+    titulo = input("Infome a Obra: \n")
+    tipo = input("Categoria: \n")
+    genero = input("Gênero:")
+
+    #Validação simples pra garantir integridade de numero inteiro cedido pelo usuario
+    try: 
+        ano = int(input("Ano de Lançamento:\n"))
+    except ValueError:
+        print("Ano invalido! Salvação padrão ativada para 0. \n")
+        ano = 0
+
+
+    status = input("Status: \n")
+
+    # segunda validação para segunda inserção de valor floatante 
+    try:
+        nota = float(input("Nota: \n"))
+    except ValueError:
+        print("Nota inserida invalida! Definida com 0.0 . \n")
+        nota = 0.0
+
+    comentario = input("Comentario: \n")
+    url_imagem = input("Link da imagem (Opcional) \n")
+    
+    #biblioteca gerenciadora de ids aleatorios para indentificador unico necessario(por hora usaremos o gerenciador aleatorio baseado no tempo ) 
+
+    import time
+    item_id = int(time.time()) # Gera um id baseado no segundo atual 
+
+    # Salvação de dados inseridos pelo usuario no nosso arquivo csv com o modolo "a" ( append)
+    with open(ARQUIVO_CSV, mode='a', newline='', encoding='utf-8') as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerow([item_id, titulo, tipo, genero, ano, status, nota, comentario, url_imagem])
+
+    print(f"\n '{titulo}' foi adicionado ao catálogo com sucesso!")
+
